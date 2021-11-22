3414870cce519d247602dd1cef5c49eee1f4de64
shamshinmax <mksmsmsn@gmail.com>
2021-11-22 21:00:40 +0300

    1, 2, 3

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 1 чилсло:");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2 чилсло:");
+int numberB = int.Parse(Console.ReadLine());
+if (numberB == numberA * numberA)
+{
+    Console.WriteLine("{0} {1} {2} {3}", "Число ", numberB, "является квадратом числа", numberA);
+}
+else {
+    Console.WriteLine("{0} {1} {2} {3}", "Число", numberB, "не является квадратом числа", numberA);
+}
+
