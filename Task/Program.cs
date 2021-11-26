e3787881b3320263c1ff601833101ef396b69f31
shamshinmax <mksmsmsn@gmail.com>
2021-11-26 17:21:49 +0300

    123

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(1, 10);
+Console.WriteLine(number);
+Console.WriteLine("{0} {1}", "Квадрат числа = ", number*number);
+
---
+++ task004/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите 3 числa:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int m = a;
+for (int i = 1; i < 4; i++){
+    if (m > b){
+        m = a;
+    }
+    else {
+        m = b;
+    }
+    if (m < c){
+        m = c;
+    }
+    
+}
+Console.WriteLine("{0} {1}", "Макс.число =", m);
